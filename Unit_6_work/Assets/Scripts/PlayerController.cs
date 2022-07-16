3a63e1242be66e1620d486994382158d9759fb0f
gwen <yinniethe1st@yahoo.com>
2022-07-16 13:19:09 -0600

    XRANGE

--- Unit_6_work/Assets/Scripts/PlayerController.cs
+++ Unit_6_work/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 
+    public float xRange;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +20,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
     // Set HorizontalInput to recieve values from keyboard
-    
+
         horizontalInput = Input.GetAxis("Horizontal");
 
     // Moves Player left and right
         transform.Translate( Vector3.right * horizontalInput * Time.deltaTime * speed );
 
-
+    // Keep player with in bounds
+    // Left side wall
+        if(transform.position.x < -xRange ) 
+        {
+            transform.position = new Vector3(-xRange,transform.position.y, transform.position.z)
+        }
+    // Right side wall
+        if(transform.position.x < xRange ) 
+        {
+            transform.position = new Vector3(xRange,transform.position.y, transform.position.z)
+        }
         
     }
 }
