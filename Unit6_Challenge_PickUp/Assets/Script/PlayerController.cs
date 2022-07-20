a859b7d6e1ba47ddf24fcd50f10857e3c18c2e94
gwen <yinniethe1st@yahoo.com>
2022-07-19 18:25:17 -0600

    Player

--- Unit6_Challenge_PickUp/Assets/Script/PlayerController.cs
+++ Unit6_Challenge_PickUp/Assets/Script/PlayerController.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizonalInput;
-    public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 50;
+
+    public float xRange = 40;
+
 
     // Update is called once per frame
     void Update()
     {
+
+        // Set HorizontalInput to recieve values from keyboard
         horizonalInput = Input.GetAxis("Horizontal");
+
+        // Moves Player left and right
+
+        transform.Translate(Vector3.right * horizonalInput * Time.deltaTime * speed);
+        // Keep player in bound
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Right side wall
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+
+
     }
 }
