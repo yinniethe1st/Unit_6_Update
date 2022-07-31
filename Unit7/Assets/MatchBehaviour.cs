e846752e7b610eb28add0c9b62c4d9f7308c3d37
gwen <yinniethe1st@yahoo.com>
2022-07-30 21:47:49 -0600

    renew unit 7

---
+++ Unit7/Assets/MatchBehaviour.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class MatchBehaviour : MonoBehaviour
+{
+    public ID idObj;
+    
+    private void OnTriggerEnter(Collider other)
+  {
+    Debug.Log(idObj);
+    }
+}
