1eeb7948355b65afe0a3ea11a978ec92099a0d9e
gwen <yinniethe1st@yahoo.com>
2022-07-30 22:48:23 -0600

    match behaviour

--- Unit7_Lab/Assets/MatchBehaviour.cs
+++ Unit7_Lab/Assets/MatchBehaviour.cs
@@ -1,10 +1,12 @@
 
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehaviour : MonoBehaviour
 {
     public ID idObj;
-    private ID otherID;
+    // private ID otherID;
+    public UnityEvent matchEvent, noMatchEvent;
     
     private void OnTriggerEnter(Collider other)
     {
@@ -13,10 +15,20 @@ public class MatchBehaviour : MonoBehaviour
     // Debug.Log(other.GetComponent<IdContainerBehaviour>().idObj);
 
         var temp0bj =  other.GetComponent<IdContainerBehaviour>();
-        if (temp0bj != null) otherID = temp0bj.idObj;
+        // if (temp0bj != null) otherID = temp0bj.idObj;
+
+        if (temp0bj == null)
+            return;
+
+        var otherID = temp0bj.idObj;
+
         if(otherID == idObj)
         {
-            Debug.Log("Matched");
+            matchEvent.Invoke();
+        }
+        else 
+        {
+            noMatchEvent.Invoke();
         }
 
     }
