5391cb76ede6b04b183c30cfdfadcf9be6d2e944
gwen <yinniethe1st@yahoo.com>
2022-08-12 19:23:30 -0600

    UPDATE

--- Unit7_Lab/Assets/Script/CoroutineBehavior.cs
+++ Unit7_Lab/Assets/Script/CoroutineBehavior.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class CoroutineBehavior : MonoBehaviour
 {
+    public bool canRun = true;
+
     // Start is called before the first frame update
     IEnumerator Start()
     {
+        Debug.Log(" start");
+
         yield return new WaitForSeconds(3);
         Debug.Log("late start");
+    
+
+    while (canRun)
+    {
+    
+        yield return new WaitForFixedUpdate();
+        Debug.Log("Run on start");
     }
+    }
+    
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Run");
         
     }
 }
