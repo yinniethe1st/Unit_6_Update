7bdc9f95689296c196725319a636d4bfbf6a58e9
gwen <yinniethe1st@yahoo.com>
2022-08-14 16:32:00 -0600

    go stay on

--- Unit7_Lab/Assets/Script/CoroutineBehavior.cs
+++ Unit7_Lab/Assets/Script/CoroutineBehavior.cs
@@ -7,47 +7,72 @@ public class CoroutineBehavior : MonoBehaviour
 {
     // public bool canRun = true;
 
-    public UnityEvent startEvent, repeatEvent, endEvent;
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, ReapeatUntilFalseEvent;
 
-
+    public bool canRun;
     public IntData counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
 
 
+    private void Start()
+    {
+        startEvent.Invoke();
+
+    }
+
+
     // Start is called before the first frame update
-    IEnumerator Start()
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    
+    private IEnumerator Counting()
     {
         
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
-        startEvent.Invoke();
+        startCountEvent.Invoke();
         yield return wfsObj;
 
     //     yield return wfsObj;
     //     Debug.Log("late start");
     
 
-    // while (canRun)
-    // {
-    
-    //     yield return wfsObj;
-    //     Debug.Log("Run on start");
-    // }
     
     while (counterNum.value > 0)
     {
         // Debug.Log(counterNum);
         yield return wfsObj;
 
-        repeatEvent.Invoke();
+        repeatCountEvent.Invoke();
         counterNum.value--;
-
       
     }
-    endEvent.Invoke();
+        endCountEvent.Invoke();
 
+    }
+
+    public void StartReapeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(ReapeatUntilFalse());
+    }
+
+
+
+    private IEnumerator ReapeatUntilFalse()
+    {
+        
+    while (canRun)
+    {
+    
+        yield return wfsObj;
+        ReapeatUntilFalseEvent.Invoke();
+
+    }
 
     }
     
