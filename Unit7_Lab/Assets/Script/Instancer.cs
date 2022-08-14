6cbf4ed5d0c733eeb8a11b2ee958119b0b66679b
gwen <yinniethe1st@yahoo.com>
2022-08-14 17:50:45 -0600

    prefab instancer

--- Unit7_Lab/Assets/Script/Instancer.cs
+++ Unit7_Lab/Assets/Script/Instancer.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Instancer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class Instancer : ScriptableObject
+{
+  
+  public void CreateInstance(GameObject obj)
+  {
+    Instantiate(obj);
+
+  }
 }
