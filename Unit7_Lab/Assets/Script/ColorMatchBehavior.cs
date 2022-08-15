16c9673c6445d703fde6728fb811b16eaed1119a
gwen <yinniethe1st@yahoo.com>
2022-08-14 19:17:31 -0600

    add call score

--- Unit7_Lab/Assets/Script/ColorMatchBehavior.cs
+++ Unit7_Lab/Assets/Script/ColorMatchBehavior.cs
@@ -1,22 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class ColorMatchBehavior : MatchBehaviour
-{
-      public ColorIdDataList ColorIdDataListObj;
+// using UnityEngine.Events;
 
-    private void Awake()
-    {
-        idObj = ColorIdDataListObj.currentColor;
-    }
+// public class ColorMatchBehavior : MatchBehaviour
+// {
+//       public ColorIdDataList ColorIdDataListObj;
 
-    public void ChangeColor(SpriteRenderer renderer)
-    {
-        var newColor = idObj as ColorID;
-        renderer.color = idObj as ColorID;
-        renderer.color = newColor.value;
-        
-    }
+//     private void Awake()
+//     {
+//         idObj = ColorIdDataListObj.currentColor;
+//     }
 
-}
+//     public void ChangeColor(SpriteRenderer renderer)
+//     {
+//         var newColor = idObj as ColorID;
+//         renderer.color = idObj as ColorID;
+//         renderer.color = newColor.value;
+
+//     }
+
+// }
